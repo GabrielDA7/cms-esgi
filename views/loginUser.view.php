4cf3b3af6c7b58e194848a91134351e73bfa9e92
SyberLeCyber <decultot.louis@gmail.com>
2018-03-25 00:57:36 +0100

    trucs

--- views/loginUser.view.php
+++ views/loginUser.view.php
@@ -1,6 +1,6 @@
 <form action="<?= DIRNAME;?>user/login" method="POST">
-	<label for="userName">Pseudo</label><input type="text" name="userName">
-	<label for="pwd">Mot de passe</label><input type="password" name="pwd">
+	<label for="userName">	Pseudo</label>			<input type="text" 		name="userName">
+	<label for="pwd">		Mot de passe</label>	<input type="password" 	name="pwd">
 	<input type="submit" name="submit" value="connexion">
 </form>
 
