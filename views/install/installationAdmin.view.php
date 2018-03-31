61340a5a225976470419179b286873efadddd9a8
SyberLeCyber <decultot.louis@gmail.com>
2018-03-31 14:42:11 +0200

    correction

--- views/install/installationAdmin.view.php
+++ views/install/installationAdmin.view.php
@@ -9,5 +9,6 @@
 	<label for="pwd">		Mot de passe </label> <input type="password" name="pwd" 	  value="aze" 		      required> </br>
 
 	<input type="hidden" name="role" value="<?=ADMIN_ROLE;?>" required> </br>
+	<input type="hidden" name="installation" value="TRUE" required> </br>
 	<input type="submit" name="submit" value="Valider">
 </form>
