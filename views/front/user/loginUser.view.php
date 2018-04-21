03903e9359978605fe5728a2c577dac0b2bb972d
Gabriel Daoud <gabriel.pro.d3@gmail.com>
2018-04-21 23:16:37 +0200

    adding sign in page

--- views/front/user/loginUser.view.php
+++ views/front/user/loginUser.view.php
@@ -1,7 +1,17 @@
-<?php $this->addModal("form", $config, $errors); ?>
-
-<?php 
-	if(isset($wrongPassword) && $wrongPassword === TRUE) {
-		echo "Mauvais identifiants";
-	}
-?>
+<section id="front-login-user" class="container-fluid">
+  <div class="row">
+      <div class="front-title">
+        <h2>Sign in</h2>
+      </div>
+  </div>
+  <div class="container main-section">
+    <?php ViewUtils::getErrors($errors) ?>
+    <div class="row M--center X--center">
+      <div class="M6 X12 login-container">
+        <div class="M12 X12">
+          <?php $this->addModal("form", $config, $errors); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
