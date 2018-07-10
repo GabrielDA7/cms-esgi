8df3ef439d1aa774a702c5709e00c163fce04ac5
Gabriel Daoud <gabriel.pro.d3@gmail.com>
2018-07-10 16:36:38 +0200

    add export bdd

--- install/uteach.sql
+++ install/uteach.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.6.6deb4
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 11 mai 2018 à 18:37
--- Version du serveur :  5.7.19
--- Version de PHP :  7.1.9
+-- Host: localhost:3306
+-- Generation Time: Jul 10, 2018 at 04:35 PM
+-- Server version: 10.1.26-MariaDB-0+deb9u1
+-- PHP Version: 7.0.30-0+deb9u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,54 +17,74 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `uteach`
+-- Database: `uteach`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `chapter`
+-- Table structure for table `chapter`
 --
 
-DROP TABLE IF EXISTS `chapter`;
-CREATE TABLE IF NOT EXISTS `chapter` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `chapter` (
+  `id` int(11) NOT NULL,
   `number` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `image` varchar(255) DEFAULT NULL,
   `trainning_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `trainning_id` (`trainning_id`),
-  KEY `trainning_id_2` (`trainning_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `dateInserted` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `chapter`
+-- Dumping data for table `chapter`
 --
 
-INSERT INTO `chapter` (`id`, `number`, `title`, `image`, `trainning_id`) VALUES
-(1, 1, 'Les bases', NULL, 1);
+INSERT INTO `chapter` (`id`, `number`, `title`, `image`, `trainning_id`, `dateInserted`) VALUES
+(1, 1, 'Les bases', NULL, 1, NULL),
+(2, 1, 'jhkj', NULL, 6, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `part`
+-- Table structure for table `comment`
 --
 
-DROP TABLE IF EXISTS `part`;
-CREATE TABLE IF NOT EXISTS `part` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `content` text NOT NULL,
+  `chapter_id` int(11) NOT NULL,
+  `trainning_id` int(11) NOT NULL,
+  `video_id` int(11) NOT NULL,
+  `comment_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`id`, `user_id`, `date`, `content`, `chapter_id`, `trainning_id`, `video_id`, `comment_id`) VALUES
+(23, 25, '2018-07-09 18:47:44', 'dsfsdf', 1, 0, 0, 0),
+(24, 25, '2018-07-10 14:11:22', 'dsfsdfs', 1, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `part`
+--
+
+CREATE TABLE `part` (
+  `id` int(11) NOT NULL,
   `number` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `author` varchar(255) NOT NULL,
   `content` text NOT NULL,
-  `chapter_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `chapter_id` (`chapter_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `chapter_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `part`
+-- Dumping data for table `part`
 --
 
 INSERT INTO `part` (`id`, `number`, `title`, `author`, `content`, `chapter_id`) VALUES
@@ -76,38 +94,33 @@ INSERT INTO `part` (`id`, `number`, `title`, `author`, `content`, `chapter_id`) 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `premium`
+-- Table structure for table `premium`
 --
 
-DROP TABLE IF EXISTS `premium`;
-CREATE TABLE IF NOT EXISTS `premium` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `premium` (
+  `id` int(11) NOT NULL,
   `startDate` date NOT NULL,
   `endDate` date NOT NULL,
-  `user_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `User_idUser` (`user_id`)
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `trainning`
+-- Table structure for table `trainning`
 --
 
-DROP TABLE IF EXISTS `trainning`;
-CREATE TABLE IF NOT EXISTS `trainning` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `trainning` (
+  `id` int(10) NOT NULL,
   `title` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `image` varchar(255) DEFAULT NULL,
   `author` varchar(255) NOT NULL,
-  `dateInserted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+  `dateInserted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `trainning`
+-- Dumping data for table `trainning`
 --
 
 INSERT INTO `trainning` (`id`, `title`, `description`, `image`, `author`, `dateInserted`) VALUES
@@ -126,17 +139,35 @@ INSERT INTO `trainning` (`id`, `title`, `description`, `image`, `author`, `dateI
 (13, 'srththcf', 'shsrhstr', NULL, 'lol', '2018-05-11 13:44:19'),
 (14, 'seg esr gesh ', 's rths rth', NULL, 'lol', '2018-05-11 13:44:22'),
 (15, 'srt hsrt hsrt h', 'h q h', NULL, 'lol', '2018-05-11 13:44:25'),
-(16, 'hqhsj trh', 'st rhsr t', NULL, 'lol', '2018-05-11 13:44:28');
+(16, 'hqhsj trh', 'st rhsr t', NULL, 'lol', '2018-05-11 13:44:28'),
+(17, 'test1', 'dsfsdf', NULL, 'gabletop', '2018-05-12 23:37:55'),
+(18, 'test2', 'dsfsdf', NULL, 'gabletop', '2018-05-12 23:37:55'),
+(19, 'test3', 'dsfsdf', NULL, 'gabletop', '2018-05-12 23:37:55'),
+(20, 'test4', 'dsfsdf', NULL, 'gabletop', '2018-05-12 23:37:55'),
+(21, 'test5', 'dsfsdf', NULL, 'gabletop', '2018-05-12 23:37:55'),
+(22, 'test6', 'dsfsdf', NULL, 'gabletop', '2018-05-12 23:37:55'),
+(23, 'test7', 'dsfsdf', NULL, 'gabletop', '2018-05-12 23:37:56'),
+(24, 'test8', 'dsfsdf', NULL, 'gabletop', '2018-05-12 23:37:56'),
+(25, 'test9', 'dsfsdf', NULL, 'gabletop', '2018-05-12 23:37:56'),
+(26, 'test10', 'dsfsdf', NULL, 'gabletop', '2018-05-12 23:37:56'),
+(27, 'test11', 'dsfsdf', NULL, 'gabletop', '2018-05-12 23:37:56'),
+(28, 'test12', 'dsfsdf', NULL, 'gabletop', '2018-05-12 23:37:56'),
+(29, 'test13', 'dsfsdf', NULL, 'gabletop', '2018-05-12 23:37:56'),
+(30, 'test14', 'dsfsdf', NULL, 'gabletop', '2018-05-12 23:37:56'),
+(31, 'test15', 'dsfsdf', NULL, 'gabletop', '2018-05-12 23:37:56'),
+(32, 'test16', 'dsfsdf', NULL, 'gabletop', '2018-05-12 23:37:56'),
+(33, 'test17', 'dsfsdf', NULL, 'gabletop', '2018-05-12 23:37:56'),
+(34, 'test18', 'dsfsdf', NULL, 'gabletop', '2018-05-12 23:37:56'),
+(35, 'test19', 'dsfsdf', NULL, 'gabletop', '2018-05-12 23:37:56');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `user`
+-- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
   `firstName` varchar(100) NOT NULL,
   `lastName` varchar(100) NOT NULL,
   `email` varchar(255) NOT NULL,
@@ -148,19 +179,18 @@ CREATE TABLE IF NOT EXISTS `user` (
   `status` tinyint(4) NOT NULL DEFAULT '0',
   `dateInserted` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `dateUpdated` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `role` int(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
+  `role` int(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `firstName`, `lastName`, `email`, `emailConfirm`, `userName`, `pwd`, `avatar`, `token`, `status`, `dateInserted`, `dateUpdated`, `role`) VALUES
 (20, 'Zqet', 'Qzetqzet', 'louis@gmail.com', '', 'aaaaa', 'da39a3ee5e6b4b0d3255bfef95601890afd80709', NULL, '2v3wh6jleww0g8k4ow804gogg4w4owcw8owc0cgsggg44s0g40', 0, '2018-03-14 12:20:10', '2018-03-30 21:41:16', 2),
 (23, 'Louis', 'Louis', 'louis@gmail.com', '', 'Lala', 'de271790913ea81742b7d31a70d85f50a3d3e5ae', NULL, '4k5qdza6baio0sg0sc4oc44ggowcc880o8ccsg044cc8skscsg', 1, '2018-03-20 19:41:10', '2018-03-30 21:48:46', 0),
 (24, 'Louis', 'Louis', 'louis@gmail.com', '', 'Lala', 'de271790913ea81742b7d31a70d85f50a3d3e5ae', NULL, '66myo5znw64gwowkcwgkg44oo4wg0ogoksswgosw00kgs44ok4', 0, '2018-03-20 19:44:31', NULL, 0),
-(25, 'Louis', 'Louis', 'louis@gmail.com', '', 'lol', 'c1a1a4b81a220cf8195aa8560bae8a332d17cfe8', NULL, '46jnr6p4tlgk4s4wc4gos0wg4kggckg08wcswko008c0cgwko4', 1, '2018-03-20 19:48:58', '2018-04-23 09:42:58', 2),
+(25, 'Louis', 'Louis', 'louis@gmail.com', '', 'lol', 'c1a1a4b81a220cf8195aa8560bae8a332d17cfe8', NULL, 'edmlo29ftzwwkg8s4w8w0cccg4ckgcwkk48kccwo88g840kcg', 1, '2018-03-20 19:48:58', '2018-04-23 09:42:58', 2),
 (26, 'LOUIS', 'LOUIS', 'louis@gmail.com', '', 'Lala', 'de271790913ea81742b7d31a70d85f50a3d3e5ae', NULL, '2whcvk8hjtq8w0kwcs8ow80sws40cccgg8gkows0oc4kkwcssg', 0, '2018-03-20 19:58:29', NULL, 0),
 (27, 'LOUIS', 'LOUIS', 'louis@gmail.com', '', 'Lala', 'de271790913ea81742b7d31a70d85f50a3d3e5ae', NULL, 'gpxzj6bqeygwsk0s0w8o8sggos04wkokw0ck88o080gswg008', 0, '2018-03-21 18:58:02', NULL, 0),
 (28, 'ARETARET', 'ERATREAT', 'louis@gmail.com', '', 'teate', 'de271790913ea81742b7d31a70d85f50a3d3e5ae', NULL, '6auoeiaxbickogc044kowwsw0c08ock4s484k8ocsoowso0k4w', 0, '2018-03-24 21:24:35', NULL, 0),
@@ -179,30 +209,113 @@ INSERT INTO `user` (`id`, `firstName`, `lastName`, `email`, `emailConfirm`, `use
 (41, 'Louis', 'Louis', 'louis@gmail.com', '', 'Lala', 'de271790913ea81742b7d31a70d85f50a3d3e5ae', NULL, '60tce6lhva4g48cc084k88wcoss48csk8s0o4wcckw4kwos4g8', 0, '2018-04-04 14:26:22', NULL, 1),
 (42, 'Louis', 'Louis', 'louis@gmail.com', '', 'jkjhkjh', 'de271790913ea81742b7d31a70d85f50a3d3e5ae', NULL, '4ln1t1em6fc4k80kk84wks40k4g84kwwgco84oswosc0s488cg', 0, '2018-04-04 14:26:54', NULL, 1),
 (43, 'Zefgizergzhg', 'Oiehfjboehoer', 'ezhfzghljzhglherg@gmail.com', '69d6ut4vyzs4sokcsckc8wc8ws8kwcck80so0kss8w4oggoo0g', 'mefhjgemohjoe', 'c1a1a4b81a220cf8195aa8560bae8a332d17cfe8', NULL, '1rislwf8mrz4gw8scggkgc88g0kwkcswkwskog4k844wk40gw4', 0, '2018-05-11 18:36:30', NULL, 1),
-(44, 'Zefgizergzhg', 'Oiehfjboehoer', 'ezhfzghljzhglherg@gmail.com', '69d6ut4vyzs4sokcsckc8wc8ws8kwcck80so0kss8w4oggoo0g', 'mefhjgemohjoe', 'c1a1a4b81a220cf8195aa8560bae8a332d17cfe8', NULL, '1rislwf8mrz4gw8scggkgc88g0kwkcswkwskog4k844wk40gw4', 0, '2018-05-11 18:36:30', NULL, 1);
+(44, 'Zefgizergzhg', 'Oiehfjboehoer', 'ezhfzghljzhglherg@gmail.com', '69d6ut4vyzs4sokcsckc8wc8ws8kwcck80so0kss8w4oggoo0g', 'mefhjgemohjoe', 'c1a1a4b81a220cf8195aa8560bae8a332d17cfe8', NULL, '1rislwf8mrz4gw8scggkgc88g0kwkcswkwskog4k844wk40gw4', 0, '2018-05-11 18:36:30', NULL, 1),
+(50, 'Gabriel', 'Daoud', 'gabrieldaoud3112@gmail.com', '1', 'D3oneLeBoss', 'c1a1a4b81a220cf8195aa8560bae8a332d17cfe8', NULL, '438gynozi0kkw8g8s0skw0oo8sg8co0owsks8k0s8g84occowo', 1, '2018-05-12 13:33:51', '2018-05-12 13:34:06', 1);
 
 --
--- Contraintes pour les tables déchargées
+-- Indexes for dumped tables
 --
 
 --
--- Contraintes pour la table `chapter`
+-- Indexes for table `chapter`
+--
+ALTER TABLE `chapter`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `trainning_id` (`trainning_id`),
+  ADD KEY `trainning_id_2` (`trainning_id`);
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `lesson_id` (`chapter_id`),
+  ADD KEY `trainning_id` (`trainning_id`),
+  ADD KEY `video_id` (`video_id`),
+  ADD KEY `comment_id` (`comment_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `part`
+--
+ALTER TABLE `part`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `chapter_id` (`chapter_id`);
+
+--
+-- Indexes for table `premium`
+--
+ALTER TABLE `premium`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `User_idUser` (`user_id`);
+
+--
+-- Indexes for table `trainning`
+--
+ALTER TABLE `trainning`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chapter`
+--
+ALTER TABLE `chapter`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+--
+-- AUTO_INCREMENT for table `part`
+--
+ALTER TABLE `part`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `premium`
+--
+ALTER TABLE `premium`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `trainning`
+--
+ALTER TABLE `trainning`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `chapter`
 --
 ALTER TABLE `chapter`
   ADD CONSTRAINT `chapter_ibfk_1` FOREIGN KEY (`trainning_id`) REFERENCES `trainning` (`id`) ON DELETE SET NULL;
 
 --
--- Contraintes pour la table `part`
+-- Constraints for table `part`
 --
 ALTER TABLE `part`
   ADD CONSTRAINT `part_ibfk_1` FOREIGN KEY (`chapter_id`) REFERENCES `chapter` (`id`) ON DELETE SET NULL;
 
 --
--- Contraintes pour la table `premium`
+-- Constraints for table `premium`
 --
 ALTER TABLE `premium`
   ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
