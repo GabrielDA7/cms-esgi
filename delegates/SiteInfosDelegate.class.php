b1333aafd1e1d4c038d68fdba0d9f185085713d4
SyberLeCyber <decultot.louis@gmail.com>
2018-07-21 22:55:00 +0200

    pages

--- delegates/SiteInfosDelegate.class.php
+++ delegates/SiteInfosDelegate.class.php
@@ -1,18 +1,21 @@
 <?php
 class SiteInfosDelegate {
 
-	private $installation;
-
-	public function __construct() {
-		$this->installation = new Installation();
-	}
-
 	public function process(&$data) {
 		$data['infos'] = $this->getSiteInfos($data);
+		$data['pages'] = $this->getPagesToShow($data);
 	}
 
 	private function getSiteInfos($data) {
-		$siteInfos = $this->installation->getAll($data)[0];
+		$installation = new Installation();
+		$siteInfos = $installation->getAll($data)[0];
 		return $siteInfos;
+	}
+
+	private function getPagesToShow($data) {
+		$page = new Page();
+		$page->setStatus(1);
+		$pages = $page->getWithParameters($data);
+		return $pages;
 	}
 }
