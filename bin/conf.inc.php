5cd28396b345b8f48c988728599e78fe7d9e90e5
Gabriel Daoud <gabriel.pro.d3@gmail.com>
2018-07-22 21:25:03 +0200

    fix

--- bin/conf.inc.php
+++ bin/conf.inc.php
@@ -13,6 +13,9 @@ define("DBHOST","localhost");
 define("DBNAME","uteach");
 define("DBPORT","3306");
 
+define("PAYPAL_CLIENT_ID", "AaQZwovZxW8pepqI1pOYSFOBt6QVadSls7cjwQ8Y3cTOZffIiM19Vb1SgnkNHIWsGl87o4bz31M4Rb7y");
+define("PAYPAL_SECRET", "EEuXx0qx4z9skUVQ_g99f9bp486j0SIyaqUqrtm1lO1ipiBjknjDW0KgPm9u37CtlNwCqLICQNctso2A");
+
 define("DS", "/");
 $scriptName=(dirname($_SERVER["SCRIPT_NAME"]) == DS)?"":dirname($_SERVER["SCRIPT_NAME"]);
 define("DIRNAME", $scriptName.DS);
